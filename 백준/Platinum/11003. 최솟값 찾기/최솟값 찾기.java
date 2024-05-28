366c4b439071bed83c5b12368524ba8a1b8f7618
macdduck <151634964+knh9612@users.noreply.github.com>
2024-05-28 18:54:10 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2440 ms, Memory: 621392 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -2,45 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws Exception {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	StringTokenizer st = new StringTokenizer(br.readLine());
-	
-	int N = Integer.parseInt(st.nextToken());
-	int L = Integer.parseInt(st.nextToken());
-	
-	st = new StringTokenizer(br.readLine());
-	Deque<Node> deq = new LinkedList<>();
-	
-	for (int i = 0; i < N; i++) {
-		// 하나씩 받으면서 그 값을 덱의 마지막 값과 비교한 다음에 넣을 것임
-		int now = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		// 덱의 마지막 값이 들어오는 값보다 크면 마지막 값 삭제
-		// 계속 반복함으로써 자동 정렬!!!!!!!!1
-		while (!deq.isEmpty() && deq.getLast().value > now) {
-			deq.removeLast();
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		// 덱에 index와 값을 저장해서 값 크기 비교 및 index로 범위 비교
+		Deque<Node> deq = new LinkedList<>();
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			// 들어오는 값
+			int input = Integer.parseInt(st.nextToken());
+			// 들어오는 값과 덱의 마지막 값 크기 비교
+			// 덱의 값이 더 크면 삭제 -> 들어오는 값이 더 작을 때 까지 삭제
+			while (!deq.isEmpty() && deq.getLast().value > input) {
+				deq.removeLast();
+			}
+			// 위의 while문으로 오름차순 정렬 됨ㅇㅇ
+			// 이제 들어오는 값 덱에 넣음
+			deq.addLast(new Node(i, input));
+			// 범위 체크
+			// 슬라이딩 윈도우의 범위가 L이내인지 확인 후 범위에 맞게 조절!
+			if (i - deq.getFirst().index >= L) {
+				deq.removeFirst();
+			}
+//			System.out.print(deq.getFirst().value + " ");
+			bw.write(deq.getFirst().value + " ");
 		}
-		// 이제 now 값 덱에 추가
-		deq.addLast(new Node(i, now));
-		// 슬라이딩 윈도우의 범위를 벗어나면 덱에서 첫번째 값 삭제
-		if (i - deq.getFirst().index >= L) {
-			deq.removeFirst();
-		}
-		bw.write(deq.getFirst().value + " ");
+		bw.flush();
+		bw.close();
+		
 	}
-	bw.flush();
-	bw.close();
 	
-	}		
-
 	static class Node {
 		int index;
 		int value;
 		
-		Node(int index, int value) {
+		public Node(int index, int value) {
 			this.index = index;
 			this.value = value;
 		}
