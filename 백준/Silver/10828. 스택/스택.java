87ccb7e97883a48fb3ffa580a8bed0478ac82e3d
macdduck <151634964+knh9612@users.noreply.github.com>
2025-04-22 22:03:00 +0900

    [Silver IV] Title: 스택, Time: 172 ms, Memory: 18756 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,43 +1,40 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<>();
-		StringBuffer sb = new StringBuffer();
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			String order = st.nextToken();
-			int num = 0;
-			if (st.hasMoreTokens()) {
-				num = Integer.parseInt(st.nextToken());
-			}
-			if (order.equals("push")) {
-				stack.push(num);
-			} else if (order.equals("pop")) {
-				if (stack.isEmpty()) {
-					sb.append("-1\n");
-				} else sb.append(stack.pop()).append("\n");
-				
-			} else if (order.equals("size")) {
-				sb.append(stack.size()).append("\n");
-				
-			} else if (order.equals("empty")) {
-				if (stack.isEmpty()) {
-					sb.append("1\n");
-				} else sb.append("0\n");
-				
-			} else if (order.equals("top")) {
-				if (stack.isEmpty()) {
-					sb.append("-1\n");
-				} else sb.append(stack.peek()).append("\n");
-				
-			}
-		}
-		System.out.println(sb.toString());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+
+            if (s.startsWith("push")) {
+                stack.push(Integer.parseInt(s.substring(5)));
+            }
+            switch (s) {
+                case "pop":
+                    sb.append(stack.isEmpty() ? -1 + "\n" : stack.pop() + "\n");
+                    break;
+                case "size":
+                    sb.append(stack.size() + "\n");
+                    break;
+                case "empty":
+                    sb.append(stack.isEmpty() ? 1 + "\n" : 0 + "\n");
+                    break;
+                case "top":
+                    sb.append(stack.isEmpty() ? -1 + "\n" : stack.peek() +"\n");
+                    break;
+            }
+        }
+        br.close();
+        System.out.println(sb.toString());
+    }
 }
