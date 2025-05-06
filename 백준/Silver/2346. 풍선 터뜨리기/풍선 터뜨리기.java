12464c33100d4068d6e21f1e104511a40e5f642e
macdduck <151634964+knh9612@users.noreply.github.com>
2025-05-06 14:03:07 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 156 ms, Memory: 15144 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -16,35 +16,31 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            dq.offerFirst(new Node(i, Integer.parseInt(st.nextToken())));
+            dq.offerLast(new Node(i, Integer.parseInt(st.nextToken())));
         }
-        // 1 <= N <= 1_000
-        Node node = dq.pollLast();
-        sb.append(node.key + " ");
-        if (dq.isEmpty()) {
-            System.out.println(1);
-            return;
-        }
+
+        Node node = dq.pollFirst();
+        sb.append(node.key).append(" ");
 
         while (!dq.isEmpty()) {
             int card = node.value;
             if (card > 0) {
                 for (int i = 0; i < card - 1; i++) {
-                    dq.offerFirst(dq.pollLast());
-                }
-                node = dq.pollLast();
-            } else {
-                for (int i = 0; i < -card - 1; i++) {
                     dq.offerLast(dq.pollFirst());
                 }
                 node = dq.pollFirst();
+            } else {
+                for (int i = 0; i < -card - 1; i++) {
+                    dq.offerFirst(dq.pollLast());
+                }
+                node = dq.pollLast();
             }
             sb.append(node.key).append(" ");
         }
+        br.close();
         System.out.println(sb);
     }
 
-    // 엥 근데 Getter()가 없어도 접근 가능하나..?
     static class Node {
         private final int key;
         private final int value;
