a814ac4229bb614fc0bc01a79e7db7457e68bbd4
macdduck <151634964+knh9612@users.noreply.github.com>
2025-06-24 18:48:23 +0900

    [Silver I] Title: 절댓값 힙, Time: 312 ms, Memory: 26448 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,34 +1,45 @@
 import java.io.*;
-import java.util.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> {
-			// 여기서 -1, 0, 1의 값이 중요한 것이 아니라 부호가 중요!
-			// 반환 값이 음수이면, 첫 번째 인자가 두 번째 인자보다 작음
-			// 반환 값이 0이면, 두 인자가 같음.
-			// 반환 값이 양수이면, 첫 번째 인자가 두 번째 인자보다 큼
-			int first_abs = Math.abs(o1);
-			int second_abs = Math.abs(o2);
-			if (first_abs == second_abs) {
-				return o1 - o2 > 0 ? 1 : -1; // 절댓값이 같을 때, o1 > o2이면 양수
-			} else {
-				return first_abs - second_abs; // 절대 값이 같지 않을 때는 크기 비교
-				// 첫 번째 값이 작으면 음수를 반환하므로 ㄱㅊ!
-				// 첫 번째 값이 크면 양수를 반환하므로 정렬 해줌
-			}
-		});
-		
-		for (int i = 0; i < N; i++) {
-			int k = Integer.parseInt(br.readLine());
-			if (k == 0) {
-				if (q.isEmpty()) System.out.println("0");
-				else System.out.println(q.poll()); 
-			} else q.add(k);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // 0, 양수 저장
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // 음수 저장
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 0; i < N; i++) {
+            int k = Integer.parseInt(br.readLine());
+
+            if (k > 0) {
+                minHeap.add(k);
+            } else if (k < 0) {
+                maxHeap.add(k);
+            } else {
+                if (minHeap.isEmpty() && maxHeap.isEmpty()) {
+                    bw.write(0 + "\n");
+                } else if (minHeap.isEmpty()) {
+                    bw.write(maxHeap.poll() + "\n");
+                } else if (maxHeap.isEmpty()) {
+                    bw.write(minHeap.poll() + "\n");
+                } else {
+                    int a = minHeap.peek();
+                    int b = -maxHeap.peek();
+
+                    if (a >= b) {
+                        bw.write(maxHeap.poll() + "\n");
+                    } else {
+                        bw.write(minHeap.poll() + "\n");
+                    }
+                }
+
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
