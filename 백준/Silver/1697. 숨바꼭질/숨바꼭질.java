64b218534c38819378c6b38cb0d835a8db22caed
macdduck <151634964+knh9612@users.noreply.github.com>
2025-08-05 13:50:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 21140 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,45 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static boolean[] visited;
-	static int[] distance;
-	static int k;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
 
-		if (n == k) {
-			System.out.println(0);
-		}
-		
-		visited = new boolean[100_001];
-		distance = new int[100_001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-		bfs(n);
-	}
+        boolean[] visited = new boolean[100_001];
+        int[] distance = new int[100_001];
 
-	private static void bfs(int start) {
-		Queue<Integer> q = new LinkedList<>();
-		q.add(start);
-		visited[start] = true;
- 		while (!q.isEmpty()) {
-			int now = q.poll();
-			for (int i : new int[] { now - 1, now + 1, now * 2 }) {
-				if (0 <= i && i <= 100_000 && !visited[i]) {
-					visited[i] = true;
-					distance[i] = distance[now] + 1;
-					q.add(i);
-					if (i == k) {
-						System.out.println(distance[i]);
-						return;
-					}
-					
-				}
-			}
-		}
-	}
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        // 시작점 예약
+        queue.add(N);
+        visited[N] = true;
+
+        while (!queue.isEmpty()) {
+            // 현재 노드 방문
+            int cur = queue.remove();
+            if (cur == K) {
+                System.out.println(distance[K]);
+            }
+
+            // 다음 노드 예약
+            for (int next : new int[]{cur - 1, cur + 1, cur * 2}) {
+                if (0 <= next && next <= 100_000 && !visited[next]) {
+                    queue.add(next);
+                    visited[next] = true;
+                    distance[next] = distance[cur] + 1;
+                }
+            }
+        }
+    }
+
 }
