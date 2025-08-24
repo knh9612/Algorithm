8f89ed181b0e0dfda05419bb7d360716bdc31f78
macdduck <151634964+knh9612@users.noreply.github.com>
2025-08-24 23:52:45 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -6,49 +6,47 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-        static List<List<Integer>> graph;
-        static boolean[] visited ;
-        static int count;
+    static List<List<Integer>> graph;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int n = Integer.parseInt(br.readLine()); // 1 <= n <= 100
-        int v = Integer.parseInt(br.readLine());
-        graph = new ArrayList<>();
-        visited = new boolean[n];
+        int count = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
         // 그래프 초기화
-        for (int i = 1; i <= n; i++) {
+        graph = new ArrayList<>();
+        for (int i = 0; i <= count; i++) {
             graph.add(new ArrayList<>());
         }
 
-        // 양방향 간선 설정
-        for (int i = 0; i < v; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+        visited = new boolean[count + 1];
 
-            graph.get(a - 1).add(b - 1);
-            graph.get(b - 1).add(a - 1);
+        // 양방향 간선 설정
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            graph.get(A).add(B);
+            graph.get(B).add(A);
         }
 
-        dfs(0);
-        System.out.println(count);
+        System.out.println(dfs(1) - 1);
 
     }
 
-    static void dfs(int cur) {
+    static int dfs(int cur) {
         // 현재 노드 방문
         visited[cur] = true;
 
-        // 다음 노드 예약
+        int count = 1;
         for (int next : graph.get(cur)) {
             if (!visited[next]) {
-                count++;
-                dfs(next);
+                count += dfs(next);
             }
         }
-
+        return count;
     }
 }
