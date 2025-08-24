f310a37244a4b30a652c9c54aab3a2064befdda6
macdduck <151634964+knh9612@users.noreply.github.com>
2025-08-25 00:09:11 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1100 ms, Memory: 75548 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -6,19 +6,17 @@ import java.util.*;
 public class Main {
     static List<List<Integer>> graph;
     static boolean[] visited;
-    static Map<Integer, Integer> answer;
+    static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        // 그래프 초기화
-        graph = new ArrayList<>();
+         graph = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
 
-        // 양방향 간선 설정
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
@@ -28,23 +26,22 @@ public class Main {
             graph.get(B).add(A);
         }
 
-        answer = new HashMap<>();
         visited = new boolean[N + 1];
+        answer = new int[N + 1];
+
         dfs(1);
-        for (int i = 2; i <= N; i++) {
-            System.out.println(answer.get(i));
+        for (int i = 2; i < answer.length; i++) {
+            System.out.println(answer[i]);
         }
 
     }
 
     static void dfs(int cur) {
-        // 현재 노드 방문
         visited[cur] = true;
 
-        // 다음 노드 예약
         for (int next : graph.get(cur)) {
             if (!visited[next]) {
-                answer.put(next, cur);
+                answer[next] = cur;
                 dfs(next);
             }
         }
