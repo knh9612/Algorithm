9074c730ad901b537a3dd648e40954818b7a5525
macdduck <151634964+knh9612@users.noreply.github.com>
2025-08-27 01:58:37 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 800 ms, Memory: 259972 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -8,18 +8,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 도시의 개수
-        int M = Integer.parseInt(st.nextToken()); // 도로의 개수
-        int K = Integer.parseInt(st.nextToken()); // 거리 정보
-        int X = Integer.parseInt(st.nextToken()); // 출발 도시 번호
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-        // graph 초기화
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
 
-        // 단방향 간선 설정
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
@@ -29,26 +27,23 @@ public class Main {
         }
 
         Queue<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[N + 1];
         int[] distance = new int[N + 1];
+        Arrays.fill(distance, -1);
+
+        queue.offer(X);
+        distance[X] = 0;
+
         List<Integer> answer = new ArrayList<>();
-
-        // 시작 노드 설정
-        queue.add(X);
-        visited[X] = true;
-
         while (!queue.isEmpty()) {
-            // 현재 노드 방문
-            int cur = queue.remove();
+            int cur = queue.poll();
             if (distance[cur] == K) {
                 answer.add(cur);
                 continue;
             }
 
             for (int next : graph.get(cur)) {
-                if (!visited[next]) {
-                    queue.add(next);
-                    visited[next] = true;
+                if (distance[next] == -1) {
+                    queue.offer(next);
                     distance[next] = distance[cur] + 1;
                 }
             }
