4f6d12032c9851b37182b9540cf81bb557ffe2ae
macdduck <151634964+knh9612@users.noreply.github.com>
2025-08-30 00:29:40 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1084 ms, Memory: 69504 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -4,15 +4,12 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static List<List<Integer>> graph;
-    static boolean[] visited;
-    static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-         graph = new ArrayList<>();
+        List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
@@ -26,24 +23,27 @@ public class Main {
             graph.get(B).add(A);
         }
 
-        visited = new boolean[N + 1];
-        answer = new int[N + 1];
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[N + 1];
+        int[] answer = new int[N + 1];
 
-        dfs(1);
-        for (int i = 2; i < answer.length; i++) {
-            System.out.println(answer[i]);
+        queue.offer(1);
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            for (int next : graph.get(cur)) {
+                if (!visited[next]) {
+                    queue.offer(next);
+                    visited[next] = true;
+                    answer[next] = cur;
+                }
+            }
         }
 
-    }
-
-    static void dfs(int cur) {
-        visited[cur] = true;
-
-        for (int next : graph.get(cur)) {
-            if (!visited[next]) {
-                answer[next] = cur;
-                dfs(next);
-            }
+        for (int i = 2; i <= N; i++) {
+            System.out.println(answer[i]);
         }
     }
 }
