ae65c2bdcb2617140f849a328ba078b088a566f4
macdduck <151634964+knh9612@users.noreply.github.com>
2025-09-15 17:55:13 +0900

    [Silver III] Title: 원상 복구 (small), Time: 476 ms, Memory: 76500 KB -BaekjoonHub

---
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] S = new int[1_000_001];
+        int[] D = new int[1_000_001];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            S[i + 1] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            D[i + 1] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] P = new int[1_1000_001];
+        for (int i = 0; i < K; i++) {
+
+            for (int j = 1; j <= N; j++) {
+                P[D[j]] = S[j];
+            }
+
+            for (int j = 1; j <= N; j++) {
+                S[j] = P[j];
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            System.out.print(P[i] + " ");
+        }
+    }
+}
