d0d0b95d138700e91d35c8e68a2bcaf46d055cac
macdduck <151634964+knh9612@users.noreply.github.com>
2025-10-07 11:58:10 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 22312 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,44 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        boolean[] visited = new boolean[N + 1];
-        List<List<Integer>> answer = new ArrayList<>();
-        backtrack(N, M, new ArrayList<>(), visited, answer);
-        StringBuilder sb = new StringBuilder();
-        for (List<Integer> list : answer) {
-            for (int i : list) {
-                sb.append(i).append(" ");
-            }
-            sb.append("\n");
-        }
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+
+        arr = new int[M];
+        visited = new boolean[N + 1];
+
+        backtrack(0);
         System.out.println(sb);
     }
 
-    public static void backtrack(int N, int M, List<Integer> curr, boolean[] visited, List<List<Integer>> answer) {
-        if (curr.size() == M) {
-            answer.add(new ArrayList<>(curr)); // 정답 저장
+    static void backtrack(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
             return;
         }
 
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                curr.add(i);
-                visited[i] = true;
-                backtrack(N, M, curr, visited, answer);
-                curr.remove(curr.size() - 1); // BackTracking: 원래 상탵로 되돌림
-                visited[i] = false;
-            }
+            if (visited[i]) continue;
+            visited[i] = true;
+            arr[depth] = i;
+            backtrack(depth + 1);
+            visited[i] = false;
         }
     }
 }
