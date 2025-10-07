06d81604ff71c52c16847d67beb1fb5b215b6f8e
macdduck <151634964+knh9612@users.noreply.github.com>
2025-10-07 13:28:21 +0900

    [Silver III] Title: N과 M (5), Time: 312 ms, Memory: 29120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] answer;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        answer = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        backtrack(0);
+        System.out.println(sb);
+    }
+
+    static void backtrack(int depth) {
+        if (depth == M) {
+            for (int i : answer) sb.append(i).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                answer[depth] = arr[i];
+                backtrack(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
