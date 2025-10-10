b55dba193fcb3a202232e5356f9ae81dc97acfe4
macdduck <151634964+knh9612@users.noreply.github.com>
2025-10-10 18:58:10 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] answer;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        answer = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        backtrack(0, 0);
+        System.out.println(sb);
+    }
+
+    static void backtrack(int start, int depth) {
+        if (depth == M) {
+            for (int i : answer) sb.append(i).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;
+
+        for (int i = start; i < N; i++) {
+            if (arr[i] == prev) continue;
+
+            prev = arr[i];
+            answer[depth] = arr[i];
+            backtrack(i + 1, depth + 1);
+        }
+    }
+}
