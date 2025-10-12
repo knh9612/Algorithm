3b9ccd8d9c8d3c9862e9226e9b7cdbe4f581dd9c
macdduck <151634964+knh9612@users.noreply.github.com>
2025-10-12 21:42:49 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,76 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
     static int[] arr;
-    static List<Integer> ops;
-    static boolean[] visited;
-    static List<List<Integer>> opsP;
+    static int[] ops;
+    static int min = 1_000_000_000;
+    static int max = -1_000_000_000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
 
         arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        ops = new int[4];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        ops = new ArrayList<>();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            int op = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < op; j++) {
-                if (i == 0) ops.add(0);
-                else if (i == 1) ops.add(1);
-                else if (i == 2) ops.add(2);
-                else ops.add(3);
-            }
+            ops[i] = Integer.parseInt(st.nextToken());
         }
 
-        visited = new boolean[ops.size()];
-        opsP = new ArrayList<>();
-        backtrack(0, new ArrayList<>());
-
-        int max = -1_000_000_000;
-        int min = 1_000_000_000;
-
-        for (List<Integer> list : opsP) {
-            int result = arr[0];
-            for (int i = 0; i < list.size(); i++) {
-                int op = list.get(i);
-                if (op == 0) result += arr[i + 1];
-                else if (op == 1) result -= arr[i + 1];
-                else if (op == 2) result *= arr[i + 1];
-                else result /= arr[i + 1];
-                if (result > 1_000_000_000 || result < -1_000_000_000) break;
-            }
-            max = Math.max(result, max);
-            min = Math.min(result, min);
-        }
+        backtrack(1, arr[0]);
         System.out.println(max);
         System.out.println(min);
+
     }
 
-    static void backtrack(int depth, List<Integer> cur) {
-        if (depth == ops.size()) {
-            opsP.add(new ArrayList<>(cur));
+    static void backtrack(int depth, int result) {
+        if (result > 1_000_000_000 || result < -1_000_000_000) return;
+        // 종료
+        if (depth == N) {
+            min = Math.min(result, min);
+            max = Math.max(result, max);
             return;
         }
 
-        for (int i = 0; i < ops.size(); i++) {
-            if (visited[i]) continue;
+        for (int i = 0; i < 4; i++) {
+            if (ops[i] <= 0) continue;
 
-            visited[i] = true;
-            cur.add(ops.get(i));
-            backtrack(depth + 1, cur);
-            visited[i] = false;
-            cur.remove(cur.size() - 1);
+            int nextResult = result;
+            if (i == 0) nextResult += arr[depth];
+            else if (i == 1) nextResult -= arr[depth];
+            else if (i == 2) nextResult *= arr[depth];
+            else nextResult /= arr[depth];
+
+            ops[i]--;
+            backtrack(depth + 1, nextResult);
+            ops[i]++;
         }
+
     }
 }
