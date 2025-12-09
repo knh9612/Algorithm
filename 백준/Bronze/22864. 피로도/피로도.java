134714073da78ab500a80968aac0b9cab86af152
macdduck <151634964+knh9612@users.noreply.github.com>
2025-12-10 00:13:55 +0900

    [Bronze II] Title: 피로도, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/22864. 피로도/피로도.java
+++ 백준/Bronze/22864. 피로도/피로도.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,40 +13,18 @@ public class Main {
         int C = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Queue<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(0, 0, 0));
+        int hp = 0;        // 피로도
+        int work = 0;     // 일량
 
-        int max = Integer.MIN_VALUE;
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            if (cur.time == 24) {
-                max = Math.max(max, cur.work);
-                continue;
+        for (int hour = 0; hour < 24; hour++) {
+            if (hp + A <= M) {   // 일할 수 있으면
+                hp += A;
+                work += B;
+            } else {             // 아니면 쉼
+                hp = Math.max(0, hp - C);
             }
-
-            // 일하기
-            if (cur.hp + A <= M) {
-                queue.offer(new Node(cur.hp + A, cur.work + B, cur.time + 1));
-            }
-
-            // 쉬기
-            int nextHp = Math.max(0, cur.hp - C);
-            queue.offer(new Node(nextHp, cur.work, cur.time + 1));
         }
 
-        System.out.println(max);
-    }
-
-    static class Node {
-        int hp;
-        int work;
-        int time;
-
-        public Node(int hp, int work, int time) {
-            this.hp = hp;
-            this.work = work;
-            this.time = time;
-        }
-
+        System.out.println(work);
     }
 }
