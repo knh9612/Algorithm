2ffdd15ccd0c115dc89f9d55734da9aef82151df
macdduck <151634964+knh9612@users.noreply.github.com>
2025-12-10 01:00:23 +0900

    [Bronze II] Title: 분해합, Time: 116 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            int k = i;
+            int sum = k;
+
+            while (k != 0) {
+                sum += k % 10;
+                k /= 10;
+            }
+
+            if (sum == N) {
+                answer = i;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
