480cb7490d6784ee59c4064d0abb3414403d4b59
macdduck <151634964+knh9612@users.noreply.github.com>
2026-02-07 15:22:53 +0900

    [Silver II] Title: DFS와 BFS, Time: 212 ms, Memory: 20500 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -4,25 +4,23 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int N, M, V;
     static List<List<Integer>> graph;
     static boolean[] visited;
-    static List<Integer> answer;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
 
         graph = new ArrayList<>();
-        visited = new boolean[N + 1];
-        // 그래프 초기화
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
 
-        // 양방향 간선 설정
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
@@ -32,33 +30,22 @@ public class Main {
             graph.get(B).add(A);
         }
 
-        answer = new ArrayList<>();
-
-        // dfs 실행
-        dfs(V);
-        // 출력 및 초기화
-        for (int i : answer) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        answer = new ArrayList<>();
+        sb = new StringBuilder();
         visited = new boolean[N + 1];
-
-        // bfs 실행
+        dfs(V);
+        sb.append("\n");
+        visited = new boolean[N + 1];
         bfs(V);
-        // 출력
-        for (int i : answer) {
-            System.out.print(i + " ");
-        }
+
+        System.out.println(sb);
     }
 
-    static void dfs(int cur) {
-        // 현재 노드 방문
-        answer.add(cur);
+    private static void dfs(int cur) {
         visited[cur] = true;
-        Collections.sort(graph.get(cur));
+        sb.append(cur).append(" ");
 
-        // 다음 노드 예약
+        Collections.sort(graph.get(cur)); // 작은 노드부터 탐색해야 하기 때문에 정렬
+
         for (int next : graph.get(cur)) {
             if (!visited[next]) {
                 dfs(next);
@@ -66,22 +53,19 @@ public class Main {
         }
     }
 
-    static void bfs(int start) {
+    private static void bfs(int start) {
         Queue<Integer> queue = new ArrayDeque<>();
-        // 초기화
-        answer.add(start);
         queue.add(start);
         visited[start] = true;
+        sb.append(start).append(" ");
 
         while (!queue.isEmpty()) {
-            // 현재 노드 꺼내서 처리
             int cur = queue.remove();
             Collections.sort(graph.get(cur));
 
-            // 다음 노드 탐색
             for (int next : graph.get(cur)) {
                 if (!visited[next]) {
-                    answer.add(next);
+                    sb.append(next).append(" ");
                     queue.add(next);
                     visited[next] = true;
                 }
