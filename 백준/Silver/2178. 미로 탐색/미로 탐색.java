d0dc25217692f321b2c302626b7abf2bdfbf27a9
macdduck <151634964+knh9612@users.noreply.github.com>
2026-02-13 01:14:21 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,47 +1,62 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] map;
-	static boolean[][] visited;
-	static int[] dx = {0, 0, -1, 1};
-	static int[] dy = {-1, 1, 0, 0};
-	static int N, M;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		visited = new boolean[N][M];
-		for (int x = 0; x < N; x++) {
-			String[] str = new String[N];
-			str = br.readLine().split("");
-			for (int y = 0; y < M; y++) {
-				map[x][y] = Integer.parseInt(str[y]);
-			}
-		}
-		bfs(0,0);
-		System.out.println(map[N-1][M-1]);
-	}
-	
-	static void bfs(int i, int j) {
-		Queue<int[]> q = new LinkedList<>(); // 큐 안에 int[]가 들어감
-		q.offer(new int[] {i, j}); // int[]이 들어가기 때문에 `new int[] {i, j}`
-		visited[i][j] = true;
-		while (!q.isEmpty()) {
-			int[] now = q.poll(); // 큐에 있는 놈 하나 빼서
-			for (int k = 0; k < 4; k++) {
-				int x = now[0] + dx[k];
-				int y = now[1] + dy[k];
-				if (0<=x && x<N && 0<=y && y<M && map[x][y]!=0 & !visited[x][y]) {
-					visited[x][y] = true;
-					map[x][y] = map[now[0]][now[1]] + 1; // depth 업데이트
-					q.add(new int[] {x, y});
-				}
-			}
-		}
-	}
+    static int N, M;
+    static int[][] map;
+    static int[][] dist;
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        dist = new int[N][M];
+
+        System.out.println(bfs(0, 0));
+    }
+
+    private static int bfs(int row, int column) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{row, column});
+        dist[row][column] = 1;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int cr = cur[0];
+            int cc = cur[1];
+
+            if (cr == N-1 && cc == M-1) {
+                return dist[cr][cc];
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nr = cr + dr[i];
+                int nc = cc + dc[i];
+
+                if (0 <= nr && nr < N && 0 <= nc && nc < M) {
+                    if (map[nr][nc] == 0 || dist[nr][nc] != 0) continue;
+
+                    queue.offer(new int[]{nr, nc});
+                    dist[nr][nc] = dist[cr][cc] + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
