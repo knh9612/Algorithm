1dd0f862f49088ebf6443fe96b59f99822efb5e4
macdduck <151634964+knh9612@users.noreply.github.com>
2026-05-24 21:03:38 +0900

    [level 2] Title: 짝지어 제거하기, Time: 22.43 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+// 20:50 start
+class Solution {
+    public int solution(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+                
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
