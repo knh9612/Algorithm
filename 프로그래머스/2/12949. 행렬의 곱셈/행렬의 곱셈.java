048ef6ecdee8d2213649773702867c2232f20d86
macdduck <151634964+knh9612@users.noreply.github.com>
2026-06-03 18:05:53 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.15 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int r1 = arr1.length;
-        int c1 = arr1[0].length;
-        int r2 = arr2.length;
-        int c2 = arr2[0].length;
-        
-        int[][] answer = new int[r1][c2];
-        
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                    
-                for (int k = 0; k < c1; k++) {
+        int row = arr1.length;
+        int column = arr2[0].length;
+
+        int[][] answer = new int[row][column];
+        // 첫 번 쨰 행렬의 row와 두 번 째 행렬의 column을 곱해서 + 연산
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                for (int k = 0; k < arr2.length; k++) {
                     answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
+
             }
         }
-        
+
         return answer;
+
     }
 }
