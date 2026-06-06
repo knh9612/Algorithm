0d87632617b9855910a2e618e7e9def4257ce34a
macdduck <151634964+knh9612@users.noreply.github.com>
2026-06-07 08:49:48 +0900

    [level 2] Title: 짝지어 제거하기, Time: 31.38 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,21 +1,22 @@
 import java.util.*;
 
-// 20:50 start
 class Solution {
     public int solution(String s) {
+        // stack 생성
         Deque<Character> stack = new ArrayDeque<>();
         
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            if (!stack.isEmpty() && stack.peek() == c) {
+        // top과 같은지 비교하면서 push
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && c == stack.peek()) {
                 stack.pop();
                 
             } else {
-                stack.push(c);
+                stack.push(c);            
             }
+                
         }
-
+        
+        // stack이 비어있으면 1, 아니면 0 return
         return stack.isEmpty() ? 1 : 0;
     }
 }
