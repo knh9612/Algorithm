1c0a51888adf7c22bf006935ee6945bda976862d
macdduck <151634964+knh9612@users.noreply.github.com>
2026-06-15 16:22:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.59 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,28 @@
 import java.util.*;
 
-// 22시 47분 시작
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // for문 2개는 시간초과라서 for문 내부에서 HashTable을 사용해서 O(1)로 접근해야 함
-        
+        // 동명이인이 있을 수 있기 때문에 해시테이블 사용
+        // Map<String, Integer> -> (이름, 동명이인 수)
         Map<String, Integer> map = new HashMap<>();
-        for (String str : completion) {
+
+        // 1. participant Map에 저장. O(N)
+        for (String str : participant) {
             map.put(str, map.getOrDefault(str, 0) + 1);
         }
-        
-        for (String str : participant) {
-            if (map.getOrDefault(str, 0) == 0) {
-                return str;
-            }
-            
+
+        // 2. completion 찾으면 -1. O(N)
+        for (String str : completion) {
             map.put(str, map.get(str) - 1);
-            
         }
-        return null;
+
+        // 3. count 수가 0이 아닌 놈이 완주하지 못한 선수 O(N)
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
+                return entry.getKey();
+            } 
+        }
+        
+        return "";
     }
 }
