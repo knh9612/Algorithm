5f5e0c3260b987099a0fc46b1eef4b186d13f7de
macdduck <151634964+knh9612@users.noreply.github.com>
2026-06-15 17:24:50 +0900

    [level 2] Title: 할인 행사, Time: 52.77 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,56 +1,32 @@
 import java.util.*;
 
-// 9시 10분 시작
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        // 아이디어:
-        // 10일 차부터 각각의 날에 구매할 수 있는 품목을 확인할 수 있도록 저장.
-        // for문을 돌면서 구매 품목 및 수량이 맞는지 확인.
-        
-        Map<String, Integer> wishList = new HashMap<>();
-        for (int i = 0; i < want.length; i++) {
-            wishList.put(want[i], number[i]);
-        }
-        
-        Map<String, Integer> discountList = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            discountList.put(discount[i], discountList.getOrDefault(discount[i], 0) + 1);
-        }
-        
-        int answer = 0;
-        int wishListSize = wishList.size();
-        int cnt = 0;
-        
-        for (Map.Entry<String, Integer> entry : wishList.entrySet()) {
-            String product = entry.getKey();
-            Integer quantity = entry.getValue();
-            
-            if (discountList.getOrDefault(product, 0) != quantity) break;   
-            cnt++;
-        }
-        if (wishListSize == cnt) answer++;
-        
-        
-        for (int i = 10; i < discount.length; i++) {
-            discountList.put(discount[i], discountList.getOrDefault(discount[i], 0) + 1);
-            discountList.put(discount[i - 10], discountList.get(discount[i - 10]) - 1);
-            
-            int count = 0;
-            for (Map.Entry<String, Integer> entry : wishList.entrySet()) {
-                String product = entry.getKey();
-                Integer quantity = entry.getValue();
-                
-                if (discountList.getOrDefault(product, 0) != quantity) break;
-                
-                count++;
+        // discount[0]부터 discount[discount.length - 10]까지 돌면서
+        // 1. 각 날짜부터 10일 간 살 수 있는 품목들을 저장.
+        // Map<Integer, Map<String, Integer> -> (index, (product, count))
+        Map<Integer, Map<String, Integer>> map = new HashMap<>();
+        for (int i = 0; i <= discount.length - 10; i++) {
+            map.put(i, new HashMap<>());
+            for (int j = i; j < i + 10; j++) {
+                map.get(i).put(discount[j], map.get(i).getOrDefault(discount[j], 0) + 1);
             }
-            
-            if (wishListSize == count) answer++;
-            
-            
         }
-        
-        
+
+        // 2. 위에서 저장한 map을 돌면서 wishList의 수량과 맞는지 확인하면서 count
+        int answer = 0;
+        A: for (Map.Entry<Integer, Map<String, Integer>> entry : map.entrySet()) {
+            Map<String, Integer> discountList = entry.getValue();
+            for (int i = 0; i < want.length; i++) {
+                String product = want[i];
+                int count = number[i];
+
+                if (!discountList.containsKey(want[i]) || discountList.get(product) != count) continue A;
+            }
+
+            answer++;
+        }
+
         return answer;
     }
 }
