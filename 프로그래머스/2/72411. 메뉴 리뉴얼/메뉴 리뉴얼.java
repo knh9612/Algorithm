10a4f23b8ebdedf6c57c96e865ae96772c920ca0
macdduck <151634964+knh9612@users.noreply.github.com>
2026-06-24 17:40:59 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 17.88 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,13 +1,16 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
+
     /*
         <문제 요구사항>
         1. 코스요리 메뉴 구성을 문자열 형태로 배열에 담아 return
         2. 정답 배열은 오름차순 정렬
         3. 최소 2명 이상의 손님들이 주문한 메뉴들로 코스요리 메뉴 구성
+
+        -- 추가 --
+        4. 최소 2명 이상의 손님들이 주문한 메뉴들 중 가장 많이 주문된 메뉴 구성만 return!!! -> 문제 제대로 읽자
     */
-    static Map<Integer, Map<String, Integer>> map;
 
     public String[] solution(String[] orders, int[] course) {
         // 필요한 자료구조는?
@@ -15,7 +18,7 @@ class Solution {
         // 다만, 조합들이 몇 번씩 나오는지 개수를 세야하므로 (r, HashMap<String, Integer>)로 수정.
         // 즉, (r, (r개를 선택하는 조합, 카운트))
 
-        map = new HashMap<>();
+        Map<Integer, Map<String, Integer>> map = new HashMap<>();
 
         // course를 돌면서 n개 인식
         for (int r : course) {
@@ -26,10 +29,10 @@ class Solution {
                 char[] orderedMenus = order.toCharArray();
                 Arrays.sort(orderedMenus);
 
-                backtrack(0, r, orderedMenus, "");
+                backtrack(0, r, orderedMenus, "", map);
             }
         }
-        
+
         // 조합들 중 count가 2 이상인 것들 중 가장 큰 count만 정답 배열에 담아 return
         List<String> answer = new ArrayList<>();
         for (int n : course) {
@@ -47,14 +50,14 @@ class Solution {
                 }
             }
         }
-        
+
         Collections.sort(answer);
-        
+
         return answer.toArray(new String[0]);
     }
 
     // 조합 함수
-    private static void backtrack(int start, int r, char[] orderedMenus, String cur) {
+    private static void backtrack(int start, int r, char[] orderedMenus, String cur, Map<Integer, Map<String, Integer>> map) {
         // 종료 조건은? 현재 개수가 r일 때
         if (cur.length() == r) {
             map.get(r).put(cur, map.get(r).getOrDefault(cur, 0) + 1);
@@ -62,7 +65,10 @@ class Solution {
         }
 
         for (int i = start; i < orderedMenus.length; i++) {
-            backtrack(i + 1, r, orderedMenus, cur +  orderedMenus[i]);
+            backtrack(i + 1, r, orderedMenus, cur +  orderedMenus[i], map);
         }
     }
+
+
+
 }
